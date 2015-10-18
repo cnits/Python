87acbe415d839c39e408c3abcec5043e702cd789
phongnhung <appstoregugle2310@gmail.com>
2015-10-18 13:07:01 +0700

    Update simple

--- Sample/BingBall_PyGame.py
+++ Sample/BingBall_PyGame.py
@@ -7,7 +7,7 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("..\BingBallPT\BingBallPT\Asset\BaseBall.png")
+ball = pygame.image.load(".\Sample\Asset\BaseBall.png")
 ballrect = ball.get_rect()
 
 while 1:
