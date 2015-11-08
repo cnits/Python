d24240a1d70cc4acda238979c8e3c7d54f747b38
phongnhung <appstoregugle2310@gmail.com>
2015-11-08 23:49:53 +0700

    Add config

---
+++ BingBallGame/BBConfig.py
@@ -0,0 +1,22 @@
+__author__ = "PhongVu"
+__date__ = "Nov 8, 2015"
+__description__ = "Set constants and configs for program"
+
+# ====== Bar ====== #
+BAR_T = 1
+BAR_R = 2
+BAR_B = 3
+BAR_L = 4
+BAR_COLOR_1 = "#00FF00"
+BAR_COLOR_2 = "#FF0000"
+
+# ====== Ball ====== #
+BALL_COLOR = "#1E90FF"
+SPEED = [5, 5]
+MIN_DELAY = 1
+MAX_DELAY = 30
+
+# ====== Board ====== #
+MIN_W = 250
+MIN_H = 150
+BOARD_COLOR = "#E1E1E1"
